787885c3f6cfb63a7b0de2ead98ddb67920e2273
Joel Kram <joelnskram@gmail.com>
2025-09-22 11:36:31 +0100

    add search bar ui comp

--- app/lib/components/Layout/Header.tsx
+++ app/lib/components/Layout/Header.tsx
@@ -1,11 +1,15 @@
 import Navbar from './Navbar';
+import SearchBar from './SearchBar';
 
 export default function Header() {
   return (
     <>
       <div className="border-2">
-        <h1 className='text-center text-4xl p-2 mb-2'>openGallery</h1>
+        <h1 className="text-center text-4xl p-2 mb-2">openGallery</h1>
         <Navbar />
+        <div className='flex justify-center p-2'>
+          <SearchBar />
+        </div>
       </div>
     </>
   );
