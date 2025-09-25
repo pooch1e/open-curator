db6555f548957c169aba2cb74d1fa315328750ff
Joel Kram <joelnskram@gmail.com>
2025-09-25 07:49:13 +0100

    add types to searchGridContainer props

--- app/lib/components/UI/SearchPage/SearchGridContainer.tsx
+++ app/lib/components/UI/SearchPage/SearchGridContainer.tsx
@@ -1,11 +1,18 @@
 import SearchGridItem from './SearchGridItem';
-export default function SearchGridContainer({ results }) {
+export default function SearchGridContainer({ results }: any) {
   return (
     <section className="pt-6 md:pt-10 gap-6 md:gap-10">
       <div className="grid grid-cols-4 gap-x-4 sm:grid-cols-12 sm:gap-x-4 md:gap-x-4 lg:grid-cols-12 lg:gap-x-6">
-        {results.map((item) => {
+        {results.map((item: any) => {
           return (
-            <SearchGridItem key={item.id} title={item.title} artist={item.artist} date={item.date} medium={item.medium} objectURL={item.objectURL} />
+            <SearchGridItem
+              key={item.id}
+              title={item.title}
+              artist={item.artist}
+              date={item.date}
+              medium={item.medium}
+              objectURL={item.objectURL}
+            />
           );
         })}
       </div>
