da61a8d5dddb7325c3879a2d2f46d2ff16896f4a
Joel Kram <joelnskram@gmail.com>
2025-10-07 20:24:39 +0100

    add pin of favourites to top of results array so favourites appear in top of screen

--- app/lib/components/UI/SearchPage/SearchGridContainer.tsx
+++ app/lib/components/UI/SearchPage/SearchGridContainer.tsx
@@ -1,9 +1,22 @@
 import SearchGridItem from './SearchGridItem';
+import { useFavourites } from '@/app/lib/hooks/useFavourites';
+
 export default function SearchGridContainer({ results }: any) {
+  const favourites = useFavourites();
+
+  const sortedResults = [...results].sort((a, b) => {
+    const isAFavourite = favourites?.favourites?.some((fav) => fav.id === a.id);
+    const isBFavourite = favourites?.favourites?.some((fav) => fav.id === b.id);
+
+    if (isAFavourite && !isBFavourite) return -1;
+    if (!isAFavourite && isBFavourite) return 1;
+    return 0;
+  });
+
   return (
     <section className="pt-6 md:pt-10 gap-6 md:gap-10">
       <div className="grid grid-cols-4 gap-x-4 sm:grid-cols-12 sm:gap-x-4 md:gap-x-4 lg:grid-cols-12 lg:gap-x-6">
-        {results.map((item: any) => {
+        {sortedResults.map((item: any) => {
           return (
             <SearchGridItem
               key={item.id}
