1c49f7a900d4361f59f330e2635c0035b6b65a10
Joel Kram <joelnskram@gmail.com>
2025-10-10 16:11:48 +0100

    refactoring main page to use cache route

--- app/page.tsx
+++ app/page.tsx
@@ -4,21 +4,23 @@ import SearchClient from './lib/components/UI/SearchPage/SearchClient';
 import { config } from '@/config';
 import { HarvardApiService } from './lib/services/HarvardMusemService';
 
-
 export default async function Page() {
+  try {
+    const fetchHarvardApi = new HarvardApiService(
+      config.harvardMuseum.baseUrl,
+      config.harvardMuseum.apiKey || ''
+    );
 
-  const fetchHarvardApi = new HarvardApiService(
-    config.harvardMuseum.baseUrl,
-    config.harvardMuseum.apiKey || ''
-  );
+    const harvardData = await fetchHarvardApi.getInitialObjectsWithImages(
+      50,
+      'rank'
+    );
 
-  const harvardData = await fetchHarvardApi.getInitialObjectsWithImages(
-    50,
-    'rank'
-  );
+    const harvardApi = await fetch('/api/cache')
 
-  // Transform Harvard data to match MuseumItem interface
-  const data = fetchHarvardApi.transformToMuseumItem(harvardData);
+    // Transform Harvard data to match MuseumItem interface
+    const data = fetchHarvardApi.transformToMuseumItem(harvardData);
 
-  return <SearchClient data={data} />;
+    return <SearchClient data={data} />;
+  } catch (err) {}
 }
