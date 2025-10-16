a37ee2d76a3e514423a0bd17e0a26eb4f6ff3cb6
Joel Kram <joelnskram@gmail.com>
2025-10-16 17:15:11 +0100

    escaped chars in erros

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
           Page Not Found
         </h2>
         <p className="text-gray-400 mb-8 font-crimson font-light">
-          The page you&apos;re looking for doesn't exist or has been moved.
+          The page you&apos;re looking for doesn&apos;t exist or has been moved.
         </p>
         <Link
           href="/"
